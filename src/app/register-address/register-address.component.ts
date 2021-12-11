cf92ffc05b221fddc266d4c2ac35c0054966b7d0
Bárbara Geovanna Lima Sol <barbarasol027@gmail.com>
2021-12-11 09:16:57 -0300

    criacao register address

--- src/app/register-address/register-address.component.ts
+++ src/app/register-address/register-address.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-address',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterAddressComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  mudarRota(){
+    this.router.navigate(['/login'])
+  }
 
   ngOnInit(): void {
   }
