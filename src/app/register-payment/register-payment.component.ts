3fce0fd7cdd94294273fc39c2ec4e72fde1f5598
Bárbara Geovanna Lima Sol <barbarasol027@gmail.com>
2021-12-11 09:18:05 -0300

    criacao register payment

--- src/app/register-payment/register-payment.component.ts
+++ src/app/register-payment/register-payment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-payment',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPaymentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  mudarRota(){
+    this.router.navigate(['/register-address'])
+  }
 
   ngOnInit(): void {
   }
